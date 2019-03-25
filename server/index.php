f3a3b80637ad5e6ed2cb028c31d0e88b167d61fc
lightyears <lightyears1998@hotmail.com>
2019-03-25 17:10:51 +0800

    使用视口

--- server/index.php
+++ server/index.php
@@ -27,6 +27,7 @@ if (isset($_REQUEST['key'])) {
 <html>
     <head>
         <title>Akane313.2</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     </head>
     <body>
         <p>Welcome to Akane313.2</p>
